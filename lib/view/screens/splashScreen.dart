28b3f592b5f5fa06c854f491c066ee2a900b89ff
unknown <mdmehedidesktop@gmail.com>
2026-07-30 00:24:32 +0600

    Update

---
+++ lib/view/screens/splashScreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:project_3/view/screens/home/home.dart';
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    timerFun();
+
+    super.initState();
+  }
+  timerFun()async{
+    await Future.delayed(Duration(seconds: 7)).then((v){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));
+
+    });
+
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDJkuGOEXVMWOxJaGa514_WFZBIhXof72G-w&s") ,),
+
+    );
+  }
+}
+
